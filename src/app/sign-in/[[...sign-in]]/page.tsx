59b197550dac75d546abdecbc5696b6bd749b5b4
alin <ccostache.alin@gmail.com>
2025-04-23 17:01:15 +0300

    Update SignInPage to change label from 'Username' to 'Email address' and improve code formatting

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -17,7 +17,7 @@ export default function SignInPage() {
           <Clerk.GlobalError className="block text-sm text-red-600" />
           <div className="space-y-4">
             <Clerk.Field name="identifier" className="space-y-2">
-              <Clerk.Label className="text-sm font-medium text-zinc-950">Username</Clerk.Label>
+              <Clerk.Label className="text-sm font-medium text-zinc-950">Email address</Clerk.Label>
               <Clerk.Input type="text" required className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400" />
               <Clerk.FieldError className="block text-sm text-red-400" />
             </Clerk.Field>
@@ -34,46 +34,25 @@ export default function SignInPage() {
             <p className="mb-4 text-center text-sm/5 text-neutral-500">Alternatively, sign in with this platforms</p>
             <div className="space-y-2">
               <Clerk.Connection name="github" className="flex w-full items-center justify-center gap-x-3 rounded-md bg-gradient-to-b from-white to-neutral-50 px-2 py-1.5 text-sm font-medium text-neutral-950 shadow outline-none ring-1 ring-black/5 hover:to-neutral-100 focus-visible:outline-offset-2 focus-visible:outline-neutral-600 active:text-neutral-950/60">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  className="size-4"
-                  aria-hidden
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="size-4" aria-hidden>
+                  <path fillRule="evenodd" clipRule="evenodd"
                     d="M12 0C5.37 0 0 5.373 0 12a12 12 0 008.208 11.387c.6.112.792-.263.792-.582v-2.04c-3.338.726-4.042-1.614-4.042-1.614-.546-1.386-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.204.084 1.837 1.237 1.837 1.237 1.07 1.833 2.809 1.304 3.495.996.107-.775.418-1.305.762-1.605-2.665-.304-5.466-1.332-5.466-5.93 0-1.31.467-2.38 1.235-3.22-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.3 1.23A11.52 11.52 0 0112 5.8c1.02.004 2.046.138 3.003.403 2.29-1.553 3.296-1.23 3.296-1.23.653 1.652.242 2.873.119 3.176.77.84 1.233 1.91 1.233 3.22 0 4.61-2.804 5.624-5.475 5.922.43.372.815 1.102.815 2.222v3.293c0 .322.19.698.8.58A12.001 12.001 0 0024 12c0-6.627-5.373-12-12-12z"
                   />
                 </svg>
-
                 Login with Github
               </Clerk.Connection>
             </div>
           </div>
-          <p className="text-center text-sm text-neutral-500">
-            Don&apos;t have an account?{' '}
-            <Clerk.Link
-              navigate="sign-up"
-              className="rounded px-1 py-0.5 text-neutral-700 outline-none hover:bg-neutral-100 focus-visible:bg-neutral-100"
-            >
+          <p className="text-center text-sm text-neutral-500">Don&apos;t have an account?{' '}
+            <Clerk.Link navigate="sign-up" className="rounded px-1 py-0.5 text-neutral-700 outline-none hover:bg-neutral-100 focus-visible:bg-neutral-100">
               Sign up
             </Clerk.Link>
           </p>
         </SignIn.Step>
-        <SignIn.Step
-          name="verifications"
-          className="w-full space-y-6 rounded-2xl px-4 py-10 sm:w-96 sm:px-8"
-        >
+        <SignIn.Step name="verifications" className="w-full space-y-6 rounded-2xl px-4 py-10 sm:w-96 sm:px-8">
           <SignIn.Strategy name="email_code">
             <header className="text-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 40 40"
-                className="mx-auto size-10"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 40 40" className="mx-auto size-10">
                 <mask id="a" width="40" height="40" x="0" y="0" maskUnits="userSpaceOnUse">
                   <circle cx="20" cy="20" r="20" fill="#D9D9D9" />
                 </mask>
@@ -82,34 +61,20 @@ export default function SignInPage() {
                   <path d="M27 3.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 8.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM23 13.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM21.5 18.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM20.5 23.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM22.5 28.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 33.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM27 38.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2Z" />
                 </g>
               </svg>
-              <h1 className="mt-4 text-xl font-medium tracking-tight text-neutral-950">
-                Verify email code
-              </h1>
+              <h1 className="mt-4 text-xl font-medium tracking-tight text-neutral-950">Verify email code</h1>
             </header>
             <Clerk.GlobalError className="block text-sm text-red-600" />
             <Clerk.Field name="code">
               <Clerk.Label className="sr-only">Email code</Clerk.Label>
-              <Clerk.Input
-                type="otp"
-                required
-                placeholder="Email code"
-                className="w-full border-b border-neutral-200 bg-white pb-2 text-sm/6 text-neutral-950 outline-none placeholder:text-neutral-400 hover:border-neutral-300 focus:border-neutral-600 data-[invalid]:border-red-600 data-[invalid]:text-red-600"
-              />
+              <Clerk.Input type="otp" required placeholder="Email code" className="w-full border-b border-neutral-200 bg-white pb-2 text-sm/6 text-neutral-950 outline-none placeholder:text-neutral-400 hover:border-neutral-300 focus:border-neutral-600 data-[invalid]:border-red-600 data-[invalid]:text-red-600" />
               <Clerk.FieldError className="mt-2 block text-xs text-red-600" />
             </Clerk.Field>
-            <SignIn.Action
-              submit
-              className="relative w-full rounded-md bg-neutral-600 bg-gradient-to-b from-neutral-500 to-neutral-600 py-1.5 text-sm text-white shadow-[0_1px_1px_0_theme(colors.white/10%)_inset,0_1px_2.5px_0_theme(colors.black/36%)] outline-none ring-1 ring-inset ring-neutral-600 before:absolute before:inset-0 before:rounded-md before:bg-white/10 before:opacity-0 hover:before:opacity-100 focus-visible:outline-offset-2 focus-visible:outline-neutral-600 active:bg-neutral-600 active:text-white/60 active:before:opacity-0"
-            >
+            <SignIn.Action submit className="relative w-full rounded-md bg-neutral-600 bg-gradient-to-b from-neutral-500 to-neutral-600 py-1.5 text-sm text-white shadow-[0_1px_1px_0_theme(colors.white/10%)_inset,0_1px_2.5px_0_theme(colors.black/36%)] outline-none ring-1 ring-inset ring-neutral-600 before:absolute before:inset-0 before:rounded-md before:bg-white/10 before:opacity-0 hover:before:opacity-100 focus-visible:outline-offset-2 focus-visible:outline-neutral-600 active:bg-neutral-600 active:text-white/60 active:before:opacity-0">
               Continue
             </SignIn.Action>
           </SignIn.Strategy>
-          <p className="text-center text-sm text-neutral-500">
-            Don&apos;t have an account?{' '}
-            <Clerk.Link
-              navigate="sign-up"
-              className="rounded px-1 py-0.5 text-neutral-700 outline-none hover:bg-neutral-100 focus-visible:bg-neutral-100"
-            >
+          <p className="text-center text-sm text-neutral-500">Don&apos;t have an account?{' '}
+            <Clerk.Link navigate="sign-up" className="rounded px-1 py-0.5 text-neutral-700 outline-none hover:bg-neutral-100 focus-visible:bg-neutral-100">
               Sign up
             </Clerk.Link>
           </p>
